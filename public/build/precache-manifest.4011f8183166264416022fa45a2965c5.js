60d61a9f659447b35057562b525a2841d5173e3f
Guadalupe Brenes <brenesmesa@gmail.com>
2020-10-01 19:07:08 -0500

    Deploy 01/10/2020

--- public/build/precache-manifest.4011f8183166264416022fa45a2965c5.js
+++ public/build/precache-manifest.4011f8183166264416022fa45a2965c5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad6d1d4c3c2e9912344ef6940a7673b5",
+    "revision": "0cd924329042afcc47b534102b45fa8c",
     "url": "/index.html"
   },
   {
-    "revision": "7fd2e85a59550b2369c5",
+    "revision": "e3b0e0350ae97a717457",
     "url": "/static/css/main.d4a9bc70.chunk.css"
   },
   {
-    "revision": "8b8dc363ec7b9f790642",
-    "url": "/static/js/2.f0411c63.chunk.js"
+    "revision": "e6386425eb0414126f6e",
+    "url": "/static/js/2.7fb75534.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.f0411c63.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7fb75534.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7fd2e85a59550b2369c5",
-    "url": "/static/js/main.95d6a548.chunk.js"
+    "revision": "e3b0e0350ae97a717457",
+    "url": "/static/js/main.86dec3d1.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0abb14f3ddb9f7a1f635fd0be53c419.js"
+  "/precache-manifest.4011f8183166264416022fa45a2965c5.js"
 );
 
 self.addEventListener('message', (event) => {
